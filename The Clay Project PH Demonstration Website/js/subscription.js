bed143c0daf13ef6279811a782ca291e3f87d088
oleanna <84418525+olyoleanna@users.noreply.github.com>
2022-10-10 16:14:53 -0700

    Delete The Clay Project PH Demonstration Website directory

--- The Clay Project PH Demonstration Website/js/subscription.js
+++
@@ -1,8 +0,0 @@
-window.onload = setForm;
-
-function setForm() {
-   document.forms[0].onsubmit = function() {
-      if (this.checkValidity()) alert("\nThis is a confirmation message.\n\nThank you for submitting your information.\n");
-      return false;
-   }
-}
